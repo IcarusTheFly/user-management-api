2698dcca0dc9fad8e33464d282c77ff55b8e1e47
IcarusTheFly <IcarusTheFly@users.noreply.github.com>
2024-10-05 19:26:58 +1000

    Tweak build and run scripts

--- packages/real-time-notifications/__tests__/real-time-notifications.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const realTimeNotifications = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(realTimeNotifications(), 'Hello from realTimeNotifications');
-console.info('realTimeNotifications tests passed');
